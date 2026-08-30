e072935133c03b22978f95830688a1ada55dce79
Ali Hesari <alihesarim@gmail.com>
2026-08-30 12:21:48 +0200

    feat(app): register the app definition with auth, triggers, creates and searches

---
+++ src/index.ts
@@ -0,0 +1,47 @@
+import { defineApp, version as platformVersion } from 'zapier-platform-core';
+
+import { authentication } from './authentication.js';
+import { addLabel } from './creates/add_label.js';
+import { createPost } from './creates/create_post.js';
+import { publishPost } from './creates/publish_post.js';
+import { uploadMedia } from './creates/upload_media.js';
+import { handleErrors, includeApiKey } from './middleware.js';
+import { findAccount } from './searches/find_account.js';
+import { findPost } from './searches/find_post.js';
+import { listAccounts } from './triggers/list_accounts.js';
+import { listLabels } from './triggers/list_labels.js';
+import { listWorkspaces } from './triggers/list_workspaces.js';
+import { newAccountConnected } from './triggers/new_account_connected.js';
+import { newPostPublished } from './triggers/new_post_published.js';
+import { postFailed } from './triggers/post_failed.js';
+import { version } from './version.js';
+
+export default defineApp({
+  version,
+  platformVersion,
+
+  authentication,
+  beforeRequest: [includeApiKey],
+  afterResponse: [handleErrors],
+
+  triggers: {
+    [newPostPublished.key]: newPostPublished,
+    [postFailed.key]: postFailed,
+    [newAccountConnected.key]: newAccountConnected,
+    [listWorkspaces.key]: listWorkspaces,
+    [listAccounts.key]: listAccounts,
+    [listLabels.key]: listLabels,
+  },
+
+  creates: {
+    [createPost.key]: createPost,
+    [publishPost.key]: publishPost,
+    [uploadMedia.key]: uploadMedia,
+    [addLabel.key]: addLabel,
+  },
+
+  searches: {
+    [findPost.key]: findPost,
+    [findAccount.key]: findAccount,
+  },
+});
